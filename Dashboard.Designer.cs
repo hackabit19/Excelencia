f27e1110286be36bea559edafff0f14efe9342eb
unknown <abhaymanmode9991@gmail.com>
2019-10-19 02:50:34 +0530

    Initial commit.

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace MultiFaceRec
+{
+    partial class Dashboard
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+     
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTransferAmount = new System.Windows.Forms.Button();
+            this.btnNewRegi = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelDashboard = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBankType = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panelBody = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblAccountNo = new System.Windows.Forms.Label();
+            this.lblIFSC = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblLoginType = new System.Windows.Forms.Label();
+            this.btnFacePay = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panelDashboard.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFacePay);
+            this.groupBox1.Controls.Add(this.btnTransferAmount);
+            this.groupBox1.Controls.Add(this.btnNewRegi);
+            this.groupBox1.Controls.Add(this.btnExit);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 651);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bank Services";
+            // 
+            // btnTransferAmount
+            // 
+            this.btnTransferAmount.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnTransferAmount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTransferAmount.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTransferAmount.Location = new System.Drawing.Point(32, 115);
+            this.btnTransferAmount.Name = "btnTransferAmount";
+            this.btnTransferAmount.Size = new System.Drawing.Size(186, 43);
+            this.btnTransferAmount.TabIndex = 8;
+            this.btnTransferAmount.Text = "Face Pay  +  Pin";
+            this.btnTransferAmount.UseVisualStyleBackColor = false;
+            this.btnTransferAmount.Click += new System.EventHandler(this.btnTransferAmount_Click);
+            // 
+            // btnNewRegi
+            // 
+            this.btnNewRegi.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnNewRegi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewRegi.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewRegi.Location = new System.Drawing.Point(32, 50);
+            this.btnNewRegi.Name = "btnNewRegi";
+            this.btnNewRegi.Size = new System.Drawing.Size(186, 44);
+            this.btnNewRegi.TabIndex = 2;
+            this.btnNewRegi.Text = "Account Registration";
+            this.btnNewRegi.UseVisualStyleBackColor = false;
+            this.btnNewRegi.Click += new System.EventHandler(this.btnNewRegi_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(32, 241);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(186, 44);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelDashboard
+            // 
+            this.panelDashboard.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelDashboard.Controls.Add(this.groupBox1);
+            this.panelDashboard.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelDashboard.Location = new System.Drawing.Point(0, 107);
+            this.panelDashboard.Name = "panelDashboard";
+            this.panelDashboard.Size = new System.Drawing.Size(272, 673);
+            this.panelDashboard.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(19, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IDEOLOGY BANK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Account Type :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(34, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "User Login :";
+            // 
+            // lblBankType
+            // 
+            this.lblBankType.AutoSize = true;
+            this.lblBankType.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBankType.ForeColor = System.Drawing.Color.Yellow;
+            this.lblBankType.Location = new System.Drawing.Point(136, 38);
+            this.lblBankType.Name = "lblBankType";
+            this.lblBankType.Size = new System.Drawing.Size(54, 21);
+            this.lblBankType.TabIndex = 2;
+            this.lblBankType.Text = "label2";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Yellow;
+            this.lblName.Location = new System.Drawing.Point(136, 17);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(54, 21);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "label2";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.lblAccountNo);
+            this.panel3.Controls.Add(this.lblIFSC);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.lblBankType);
+            this.panel3.Controls.Add(this.lblName);
+            this.panel3.Location = new System.Drawing.Point(688, 17);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(835, 73);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1541, 107);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Gold;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 780);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1541, 50);
+            this.panel2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(684, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(225, 21);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Powerd By : Science Ideology";
+            // 
+            // panelBody
+            // 
+            this.panelBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBody.Location = new System.Drawing.Point(272, 107);
+            this.panelBody.Name = "panelBody";
+            this.panelBody.Size = new System.Drawing.Size(1269, 673);
+            this.panelBody.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(12, 17);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(303, 73);
+            this.panel4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(512, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Account No. :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(526, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 21);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "IFSC CODE :";
+            // 
+            // lblAccountNo
+            // 
+            this.lblAccountNo.AutoSize = true;
+            this.lblAccountNo.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccountNo.ForeColor = System.Drawing.Color.Yellow;
+            this.lblAccountNo.Location = new System.Drawing.Point(636, 38);
+            this.lblAccountNo.Name = "lblAccountNo";
+            this.lblAccountNo.Size = new System.Drawing.Size(54, 21);
+            this.lblAccountNo.TabIndex = 6;
+            this.lblAccountNo.Text = "label2";
+            // 
+            // lblIFSC
+            // 
+            this.lblIFSC.AutoSize = true;
+            this.lblIFSC.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIFSC.ForeColor = System.Drawing.Color.Yellow;
+            this.lblIFSC.Location = new System.Drawing.Point(636, 17);
+            this.lblIFSC.Name = "lblIFSC";
+            this.lblIFSC.Size = new System.Drawing.Size(54, 21);
+            this.lblIFSC.TabIndex = 5;
+            this.lblIFSC.Text = "label2";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel5.Controls.Add(this.lblLoginType);
+            this.panel5.Location = new System.Drawing.Point(350, 17);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(303, 73);
+            this.panel5.TabIndex = 6;
+            // 
+            // lblLoginType
+            // 
+            this.lblLoginType.AutoSize = true;
+            this.lblLoginType.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginType.ForeColor = System.Drawing.Color.Gold;
+            this.lblLoginType.Location = new System.Drawing.Point(6, 10);
+            this.lblLoginType.Name = "lblLoginType";
+            this.lblLoginType.Size = new System.Drawing.Size(168, 45);
+            this.lblLoginType.TabIndex = 0;
+            this.lblLoginType.Text = "User Type";
+            // 
+            // btnFacePay
+            // 
+            this.btnFacePay.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnFacePay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFacePay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFacePay.Location = new System.Drawing.Point(32, 178);
+            this.btnFacePay.Name = "btnFacePay";
+            this.btnFacePay.Size = new System.Drawing.Size(186, 47);
+            this.btnFacePay.TabIndex = 9;
+            this.btnFacePay.Text = "Face Pay";
+            this.btnFacePay.UseVisualStyleBackColor = false;
+            this.btnFacePay.Click += new System.EventHandler(this.btnFacePay_Click);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1541, 830);
+            this.Controls.Add(this.panelBody);
+            this.Controls.Add(this.panelDashboard);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panelDashboard.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnNewRegi;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelDashboard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBankType;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panelBody;
+        private System.Windows.Forms.Button btnTransferAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblAccountNo;
+        private System.Windows.Forms.Label lblIFSC;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblLoginType;
+        private System.Windows.Forms.Button btnFacePay;
+    }
+}
